96f5ba89829b04c9d27f8dc60a337723cef7162d
Steve Hook <steevehook@gmail.com>
2025-05-19 09:10:19 +0300

    feat: add wallets tests

--- pkg/wallets/wallets_test.go
+++ pkg/wallets/wallets_test.go
@@ -1,7 +1,51 @@
 package wallets
 
-import "github.com/stretchr/testify/suite"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
 
 type walletsSuite struct {
 	suite.Suite
+	wallet *Wallet
+}
+
+func (s *walletsSuite) SetupTest() {
+	s.wallet = &Wallet{
+		ID: "12345",
+	}
+}
+
+func (s *walletsSuite) Test_NewWallet() {
+	wallet := NewWallet("12345")
+
+	s.Equal("12345", wallet.ID)
+	s.Equal(0.0, wallet.balance)
+}
+
+func (s *walletsSuite) Test_Deposit() {
+	s.wallet.Deposit(100.0)
+
+	s.Equal(100.0, s.wallet.balance)
+}
+
+func (s *walletsSuite) Test_Withdraw() {
+	s.wallet.balance = 100.0
+
+	s.wallet.Withdraw(50.0)
+
+	s.Equal(50.0, s.wallet.balance)
+}
+
+func (s *walletsSuite) Test_GetBalance() {
+	s.wallet.balance = 100.0
+
+	balance := s.wallet.GetBalance()
+
+	s.Equal(100.0, balance)
+}
+
+func TestWallets(t *testing.T) {
+	suite.Run(t, new(walletsSuite))
 }
