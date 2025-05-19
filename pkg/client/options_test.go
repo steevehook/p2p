42e63469773ec4e6ed88128e871db3dead4ab31d
Steve Hook <steevehook@gmail.com>
2025-05-19 10:21:30 +0300

    feat: add test for client options

--- pkg/client/options_test.go
+++ pkg/client/options_test.go
@@ -1,1 +1,16 @@
 package client
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithServerAddress(t *testing.T) {
+	expectedAddress := "localhost:8000"
+	client := &Client{}
+
+	WithServerAddress(expectedAddress).apply(client)
+
+	assert.Equal(t, expectedAddress, client.address)
+}
