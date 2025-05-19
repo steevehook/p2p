c122199d4046074811ee9f8f3b35743bff043df4
Steve Hook <steevehook@gmail.com>
2025-05-19 10:20:07 +0300

    feat: add test for server options

--- pkg/server/options_test.go
+++ pkg/server/options_test.go
@@ -1,1 +1,16 @@
 package server
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithPort(t *testing.T) {
+	expectedPort := 8000
+	srv := &Server{}
+
+	WithPort(expectedPort).apply(srv)
+
+	assert.Equal(t, expectedPort, srv.port)
+}
